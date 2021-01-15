f301230558fd0256ccdb42d1bb0e1ea127ad9142
Mimioj101 <mimioj@gmail.com>
2021-01-15 12:06:05 -0500

    searchbar up on page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchForm from './components/search.js'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <SearchForm/>
       </header>
     </div>
   );
--- src/components/search.js
+++ src/components/search.js
@@ -1,1 +1,31 @@
-import react from 'React'
+import React from 'react';
+
+class SearchForm extends React.Component {
+
+    state = {
+        search: ""
+    }
+    
+    
+    changeHandler = (e) => {
+        e.persist();
+        this.setState({search: e.target.value})
+        // this.props.searchHandler(e.target.value)
+    }
+    
+    
+    render() {
+        return(
+        <div className="ui search">
+            <div className="ui icon input">
+                <input className="prompt" onChange={this.changeHandler} value={this.state.search} placeholder="Search books by title"/>
+                <i className="search icon" />
+            </div>
+        </div>
+        )
+    }
+
+}
+
+
+export default SearchForm;
