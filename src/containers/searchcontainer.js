4324446767ab416023ce87242d28c127646e09da
Mimioj101 <mimioj@gmail.com>
2021-01-26 11:48:44 -0500

    SearchContainer class

---
+++ src/containers/searchcontainer.js
@@ -0,0 +1,14 @@
+class SearchContainer extends React.Component {
+    
+    
+    render() {
+        return(
+        <div className="search-container">
+        </div>
+        )
+    }
+
+}
+
+
+export default SearchContainer;
